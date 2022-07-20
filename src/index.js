76a53d5a291671de87bdb58d21714b6d2dce5c76
Ankit-Goswami <ankitgoswami.work@gmail.com>
2022-07-20 16:03:19 +0530

    env file added

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+require('dotenv').config()
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log("MongoDb is Connected"))
+  .catch((err) => console.log(err));
 
-mongoose.connect("mongodb+srv://DbProject:UXvJ3hrjdhmVjFq1@cluster0.jcr2s.mongodb.net/Project1-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
 
 app.use('/', route);
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000,  () => {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
